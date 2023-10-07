afcfa30e4a5fc423dac53191da13ab76e17447af
Rafał Świeczkowski <rswieczkowski@leonsoftware.com>
2023-10-07 17:55:25 +0200

    Add removing items functionality

--- js/app.js
+++ js/app.js
@@ -40,6 +40,7 @@ class CalorieTracker {
       this._totalCalories -= meal.calories;
       Storage.updateTotalCalories(this._totalCalories);
       this._meals.splice(index, 1);
+      Storage.removeMeal(meal);
       this._render();
     }
   }
@@ -52,6 +53,7 @@ class CalorieTracker {
       this._totalCalories += workout.calories;
       Storage.updateTotalCalories(this._totalCalories);
       this._workouts.splice(index, 1);
+      Storage.removeWorkout(id);
       this._render();
     }
   }
@@ -271,9 +273,31 @@ class Storage {
     localStorage.setItem('meals', JSON.stringify(meals));
   }
 
+  static removeMeal(id) {
+    const meals = Storage.getMeals();
+
+    meals.forEach((meal, index) => {
+      if (id === meal.id) {
+        meals.splice(index, 1);
+      }
+    });
+    localStorage.setItem('meals', JSON.stringify(meals));
+  }
+
   static saveWorkout(workout) {
     const workouts = Storage.getWorkouts();
     workouts.push(workout);
+    localStorage.setItem('workouts', JSON.stringify(workouts));
+  }
+
+  static removeWorkout(id) {
+    const workouts = Storage.getWorkouts();
+
+    workouts.forEach((workout, index) => {
+      if (id === workout.id) {
+        workouts.splice(index, 1);
+      }
+    });
     localStorage.setItem('workouts', JSON.stringify(workouts));
   }
 }
